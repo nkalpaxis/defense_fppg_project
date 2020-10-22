e82d40944b8eb8857eeec191f7062d820d9f65e0
nkalpaxis <58003892+nkalpaxis@users.noreply.github.com>
2020-10-22 17:00:54 -0400

    Update football.py
    added clean_team function to convert team names into the abbreviated team names // removed lines that were commented out used in testing

--- football.py
+++ football.py
@@ -10,8 +10,6 @@ data = list(r)
 header = data[0]
 data = data[1:]
 
-# print(header)
-
 
 # a function that extracts entire columns for us
 def extract(index):
@@ -21,8 +19,6 @@ def extract(index):
         column.append(col)
     return column
 
-qb_ppg = extract(2)
-# print(qb_ppg)
 
 # function that converts extracted string values to floats and then integers
 def convert_to_int(a_list):
@@ -34,7 +30,6 @@ def convert_to_int(a_list):
         converted.append(v)
     return converted
 
-# print(convert_to_int(qb_ppg))
 
 # function that combines both extract and convert_to_int function just pass in the index you want to extract and an int list of the values will be returned
 def extract_and_convert(index):
@@ -42,5 +37,14 @@ def extract_and_convert(index):
     conversion = convert_to_int(column)
     return conversion
 
-print(extract_and_convert(2))
-print(extract_and_convert(3))
+# function that takes in a team name, splits it and upper cases the first 3 characters
+# if the team name has 3 words ex: New York Giants; it will split and take the first character of each string and upper case them
+def clean_team(team):
+    team = team.split()
+    if len(team) == 3:
+        first_three = team[0][0] + team[1][0] + team[2][0]
+    else:
+        first_three = team[0][:3]
+
+    first_three = first_three.upper()
+    return first_three
